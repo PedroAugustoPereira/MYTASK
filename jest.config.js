c9145b1fc47de9a75c81452289125e929a9aa413
Pedro Augusto Pereira <pereiraaugustopedro2021@gmail.com>
2023-08-31 12:09:59 -0300

    Configuração do husky, prettier, eslint, e primeiros models bem como primeiro controller, inicio de autenticação

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   roots: ["<rootDir>/src"], //diretório raiz
   collectConverageFrom: ["<rootDir>/src/**/*.ts"], //onde ele vai pegar o arquivo
-  coverageDirectory: "converage", //onde vai jogar o arquivo
   testEnvironment: "node", //linguagem node
   transform: {
     //semppre que usar o ts usar o ts-jest
